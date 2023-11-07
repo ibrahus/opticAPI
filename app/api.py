7c9c615af132dd2df0417b749c5786c4f55e0641
i.alhussayen <i.alhussayen@lean.sa>
2023-11-07 10:14:36 +0300

    refactor

--- app/api.py
+++ app/api.py
@@ -1,20 +1,21 @@
 import logging
 import os
+import shutil
 from fastapi import APIRouter, BackgroundTasks, File, HTTPException, UploadFile
 from fastapi.responses import FileResponse, JSONResponse
 import tempfile
+from typing import Tuple
 import replicate
 from deep_translator import GoogleTranslator
 from gtts import gTTS
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 
-load_dotenv(find_dotenv())  # Read local .env file
-
-router = APIRouter()
-
+# Setup environment and logger
+load_dotenv()  # This automatically finds the .env file
 logger = logging.getLogger(__name__)
 
-# The root directory for audio files
+# Configure APIRouter and constants
+router = APIRouter()
 AUDIO_FILES_DIRECTORY = "audio-files"
 TRANSLATOR = GoogleTranslator(source='en', target='ar')
 MODEL = "yorickvp/llava-13b:2facb4a474a0462c15041b78b1ad70952ea46b5ec6ad29583c0b29dbd4249591"
@@ -25,7 +26,6 @@ async def health():
     return {"status": "ok"}
 
 
-# Helper function to delete a file
 def remove_file(file_path: str):
     try:
         os.remove(file_path)
@@ -33,113 +33,79 @@ def remove_file(file_path: str):
         logger.error(f"Failed to delete {file_path}: {e}")
 
 
+async def save_upload_file(upload_file: UploadFile) -> str:
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        shutil.copyfileobj(upload_file.file, temp_file)
+        temp_file_path = temp_file.name
+    return temp_file_path
+
+
+async def process_image(file_path: str, prompt: str) -> Tuple[str, str]:
+    with open(file_path, "rb") as image_file:
+        output = replicate.run(
+            MODEL, input={"image": image_file, "prompt": prompt})
+    full_description = "".join(output)
+    translation = TRANSLATOR.translate(full_description)
+    return full_description, translation
+
+
+async def create_audio_file(translation: str) -> str:
+    tts = gTTS(text=translation, lang='ar')
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp:
+        tts.save(temp.name)
+    return temp.name
+
+
 @router.post("/chat-with-image")
 async def chat_with_image(file: UploadFile = File(...), prompt: str = ""):
-    # Using 'with' statements ensures proper resource management, especially for I/O operations.
     try:
-        with tempfile.TemporaryDirectory() as tempdir:
-            temp_file_path = os.path.join(tempdir, file.filename)
-            with open(temp_file_path, "wb") as buffer:
-                # Directly saving the incoming file data, avoiding reading it into memory.
-                buffer.write(await file.read())
-
-            # Avoid reopening the file by keeping it open and passing the file object directly.
-            with open(temp_file_path, "rb") as image_file:
-                # Not clear what 'replicate' does as it's not a standard library. Assuming it's a third-party service for processing.
-                output = replicate.run(
-                    MODEL, input={"image": image_file, "prompt": prompt})
-
-            # 'output' is assumed to be a list; if it's not, the join operation will fail.
-            full_description = "".join(output)
-
-            translation = TRANSLATOR.translate(full_description)
-
-        tts = gTTS(text=translation, lang='ar')
-
-        # Using 'with' for automatic file handling.
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp:
-            # Directly using 'temp.name' as 'tts.save' doesn't require file object, but the name.
-            tts.save(temp.name)
-            # Storing the file name for the response.
-            temp_file_name = temp.name
-
-        return FileResponse(temp_file_name, media_type="audio/mpeg")
-
+        file_path = await save_upload_file(file)
+        full_description, translation = await process_image(file_path, prompt)
+        audio_file_path = await create_audio_file(translation)
+        return FileResponse(audio_file_path, media_type="audio/mpeg", filename=os.path.basename(audio_file_path))
     except Exception as e:
         logger.error(
             f"An error occurred during processing: {e}", exc_info=True)
-        raise HTTPException(
-            status_code=500, detail="An error occurred during processing.") from e
-
+        raise HTTPException(status_code=500, detail=str(e))
     finally:
-        # Ensure the file is closed even if an error occurs.
+        remove_file(file_path)
         await file.close()
 
 
 @router.post("/chat-with-image-test")
 async def chat_with_image_test(file: UploadFile = File(...), prompt: str = ""):
-    # Using 'with' statements ensures proper resource management, especially for I/O operations.
     try:
-        with tempfile.TemporaryDirectory() as tempdir:
-            temp_file_path = os.path.join(tempdir, file.filename)
-            with open(temp_file_path, "wb") as buffer:
-                # Directly saving the incoming file data, avoiding reading it into memory.
-                buffer.write(await file.read())
-
-            # Avoid reopening the file by keeping it open and passing the file object directly.
-            with open(temp_file_path, "rb") as image_file:
-                # Not clear what 'replicate' does as it's not a standard library. Assuming it's a third-party service for processing.
-                output = replicate.run(
-                    MODEL, input={"image": image_file, "prompt": prompt})
-
-            # 'output' is assumed to be a list; if it's not, the join operation will fail.
-            full_description = "".join(output)
-
-            translation = TRANSLATOR.translate(full_description)
-
-        tts = gTTS(text=translation, lang='ar')
+        file_path = await save_upload_file(file)
+        full_description, translation = await process_image(file_path, prompt)
 
         os.makedirs(AUDIO_FILES_DIRECTORY, exist_ok=True)
-        # Using 'with' for automatic file handling.
-        with tempfile.NamedTemporaryFile(delete=False, dir=AUDIO_FILES_DIRECTORY, suffix='.mp3') as temp:
-            # Directly using 'temp.name' as 'tts.save' doesn't require file object, but the name.
-            tts.save(temp.name)
-            # Storing the relative file path for the response.
-            relative_file_path = os.path.relpath(
-                temp.name, start=AUDIO_FILES_DIRECTORY)
+        audio_file_path = await create_audio_file(translation)
+        audio_file_name = os.path.basename(audio_file_path)
+        final_path = os.path.join(AUDIO_FILES_DIRECTORY, audio_file_name)
+        shutil.move(audio_file_path, final_path)
 
-            audio_url = relative_file_path
-
-            response_data = {
-                "audio_url": audio_url,
-                "text": full_description,
-                "translated_text": translation,
-            }
-
-            return JSONResponse(content=response_data)
-
+        response_data = {
+            "audio_url": audio_file_name,
+            "text": full_description,
+            "translated_text": translation,
+        }
+        return JSONResponse(content=response_data)
     except Exception as e:
         logger.error(
             f"An error occurred during processing: {e}", exc_info=True)
-        raise HTTPException(
-            status_code=500, detail="An error occurred during processing.") from e
-
+        raise HTTPException(status_code=500, detail=str(e))
     finally:
-        # Ensure the file is closed even if an error occurs.
+        remove_file(file_path)
         await file.close()
 
 
-# You will need to add an endpoint to serve the audio file if it's not already present
 @router.get("/download/{filename}")
 async def download_audio(background_tasks: BackgroundTasks, filename: str):
     file_path = os.path.join(AUDIO_FILES_DIRECTORY, filename)
     if os.path.isfile(file_path):
         response = FileResponse(
             path=file_path, media_type='audio/mpeg', filename=filename)
-
-        # Add background task to delete the file after sending the response
         background_tasks.add_task(remove_file, file_path)
-
         return response
     else:
         raise HTTPException(status_code=404, detail="File not found.")
