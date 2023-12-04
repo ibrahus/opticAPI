d32475a4dd2ecaf923d19b4caadaaccb21a4b739
i.alhussayen <i.alhussayen@lean.sa>
2023-12-04 18:39:10 +0300

    revert database url

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
-from sqlalchemy.engine import URL
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+
 from app.models import Base
 
 # Load environment variables
@@ -11,20 +11,13 @@ load_dotenv()
 # Database configurations
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost/optic_db")
 
-if DATABASE_URL:
-    # Convert string URL to URL object, which helps in proper handling of parameters
-    url = URL.create(DATABASE_URL, query={"sslmode": "disable"})
+if DATABASE_URL and "postgresql://" in DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
 
-    # Replace 'postgresql://' with 'postgresql+asyncpg://'
-    if url.drivername == "postgresql":
-        url = url.set(drivername="postgresql+asyncpg")
-
-    engine = create_async_engine(url, echo=False)
-else:
-    raise ValueError("No DATABASE_URL found in environment variables")
-
+engine = create_async_engine(DATABASE_URL, echo=False)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 async def init_db():
     async with engine.begin() as conn:
+        # Create tables if they don't exist
         await conn.run_sync(Base.metadata.create_all)
